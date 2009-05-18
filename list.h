1c69dcb680c3c206bbc8c8c4b778bded4f49f285
Yoann Canal <yoann@pps.jussieu.fr>
2009-05-18 15:44:57 +0000

    indentation and spaces corrections

--- list.h
+++ list.h
@@ -17,7 +17,6 @@ typedef struct tr_list{
 
 typedef struct piece{
     /* TO-DO: add a file field for multi-files torrents */
-    /* offset associated with a chunk in a file */
     int64_t offset;
     int begin;
     int length;
@@ -26,23 +25,14 @@ typedef struct piece{
 } piece;
 
 typedef struct flist{
-    /**
-       flag == 0 :
-         nothing to do write must die
-       flag == 1 :  
-         the list is not emtpy so writer must countinue
-        to execute.
-	flag == 2 :
-	  Is this realy needed?
-     */
-    int flag;
+    int flag; /* exist a stream_writer */
     ht_torrent *t;
     piece *list;
 } flist;
 
 
-void tr_insert(ht_torrent * t, char *url);
-piece* add_piece(piece *l, int64_t offset, int begin, int length);
-piece* remove_piece(piece *l, int offset, int begin, int length);
+void tr_insert(ht_torrent *t, char *url);
+piece * add_piece(piece *l, int64_t offset, int begin, int length);
+piece * remove_piece(piece *l, int offset, int begin, int length);
 
 #endif
