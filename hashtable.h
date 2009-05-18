50a946b772b0c1894729b786d85d7f38cd1fff4b
Yoann Canal <yoann@pps.jussieu.fr>
2009-05-18 14:14:29 +0000

    remove pieces_list structure

--- hashtable.h
+++ hashtable.h
@@ -39,15 +39,6 @@ typedef struct ht_torrent{
 } ht_torrent;
 
 
-typedef struct piece{
-    /* TO-DO: add a file field for multi-files torrents */
-    /* offset associated with a chunk in a file */
-    int64_t offset;
-    int begin;
-    int length;
-
-}piece;
-
 int ht_load(hashtable * table, char *curr_path, benc *raw);
 int ht_info_load(ht_torrent * elmt, char *curr_path, benc *raw);
 #endif
