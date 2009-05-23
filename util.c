15e4df97c02ad159bb9b22505d897627eada6bf9
Juliusz Chroboczek <jch@pps.jussieu.fr>
2009-05-23 00:09:53 +0000

    Implement incore. This function returns true if the given memory range is in core (as opposed to having to be fetched from disk).

--- util.c
+++ util.c
@@ -1,3 +1,4 @@
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/mman.h>
 
@@ -13,4 +14,27 @@ prefetch(void *begin, size_t length)
     size_t l = length + (b - br);
     size_t lr = (l + (pagesize - 1)) / pagesize * pagesize;
     return posix_madvise((void*)br, lr, POSIX_MADV_WILLNEED);
+}
+
+int
+incore(void *begin, size_t length)
+{
+    size_t b = (size_t)begin;
+    size_t br = b / pagesize * pagesize;
+    size_t l = length + (b - br);
+    size_t lr = (l + (pagesize - 1)) / pagesize * pagesize;
+    unsigned char vec[32];
+    int i, rc;
+
+    if(lr > 32 * pagesize)
+        return -1;
+
+    rc = mincore((void*)br, lr, vec);
+    if(rc < 0)
+        return -1;
+
+    for(i = 0; i < lr / pagesize; i++)
+        if(!(vec[i] & 1))
+            return 0;
+    return 1;
 }
